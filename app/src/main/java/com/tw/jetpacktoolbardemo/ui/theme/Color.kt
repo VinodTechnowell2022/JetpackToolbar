af2ba765bbc401871203e3a74ad3a882db582b5e
vinodgaur1911 <vinodgaur1911@gmail.com>
2024-09-25 23:48:33 +0530

    initial commit

---
+++ app/src/main/java/com/tw/jetpacktoolbardemo/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.tw.jetpacktoolbardemo.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+//no chages just color code may changes as per needs
+val Purple80 = Color(0xFF230172)
+val PurpleGrey80 = Color(0xFF3D0695)
+val Pink80 = Color(0xFF8B052C)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
